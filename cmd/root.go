d92722b5b4540c44f0f55ecc65f72f79717d06f2
Ben Wilson <doubtingben@gmail.com>
2020-01-07 21:10:48 -0500

    Move server startup to rootcmd

--- cmd/root.go
+++ cmd/root.go
@@ -2,17 +2,28 @@ package cmd
 
 import (
 	"fmt"
+	"net"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/reflection"
 
 	"github.com/zcash-hackworks/lightwalletd/common"
+	"github.com/zcash-hackworks/lightwalletd/common/logging"
+	"github.com/zcash-hackworks/lightwalletd/frontend"
+	"github.com/zcash-hackworks/lightwalletd/walletrpc"
 )
 
 var cfgFile string
+var log *logrus.Entry
+var logger = logrus.New()
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -25,7 +36,7 @@ var rootCmd = &cobra.Command{
 			BindAddr:      viper.GetString("bind-addr"),
 			TLSCertPath:   viper.GetString("tls-cert"),
 			TLSKeyPath:    viper.GetString("tls-key"),
-			LogLevel:      viper.GetInt("log-level"),
+			LogLevel:      viper.GetUint64("log-level"),
 			LogFile:       viper.GetString("log-file"),
 			ZcashConfPath: viper.GetString("zcash-conf-path"),
 			VeryInsecure:  viper.GetBool("very-insecure"),
@@ -33,7 +44,107 @@ var rootCmd = &cobra.Command{
 		}
 
 		fmt.Printf("Options: %#v", opts)
+		logger.SetLevel(logrus.Level(opts.LogLevel))
+
+		// Start server annd block, or exit
+		if err := startServer(opts); err != nil {
+			log.WithFields(logrus.Fields{
+				"error": err,
+			}).Fatal("couldn't create server")
+		}
 	},
+}
+
+func startServer(opts *common.Options) error {
+	// gRPC initialization
+	var server *grpc.Server
+
+	if opts.VeryInsecure {
+		server = grpc.NewServer(logging.LoggingInterceptor())
+	} else {
+		transportCreds, err := credentials.NewServerTLSFromFile(opts.TLSCertPath, opts.TLSKeyPath)
+		if err != nil {
+			log.WithFields(logrus.Fields{
+				"cert_file": opts.TLSCertPath,
+				"key_path":  opts.TLSKeyPath,
+				"error":     err,
+			}).Fatal("couldn't load TLS credentials")
+		}
+		server = grpc.NewServer(grpc.Creds(transportCreds), logging.LoggingInterceptor())
+	}
+
+	// Enable reflection for debugging
+	if opts.LogLevel >= uint64(logrus.WarnLevel) {
+		reflection.Register(server)
+	}
+
+	// Initialize Zcash RPC client. Right now (Jan 2018) this is only for
+	// sending transactions, but in the future it could back a different type
+	// of block streamer.
+
+	rpcClient, err := frontend.NewZRPCFromConf(opts.ZcashConfPath)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"error": err,
+		}).Fatal("setting up RPC connection to zcashd")
+	}
+
+	// Get the sapling activation height from the RPC
+	// (this first RPC also verifies that we can communicate with zcashd)
+	saplingHeight, blockHeight, chainName, branchID := common.GetSaplingInfo(rpcClient, log)
+	log.Info("Got sapling height ", saplingHeight, " chain ", chainName, " branchID ", branchID)
+
+	// Initialize the cache
+	cache := common.NewBlockCache(opts.CacheSize)
+
+	// Start the block cache importer at cacheSize blocks before current height
+	cacheStart := blockHeight - opts.CacheSize
+	if cacheStart < saplingHeight {
+		cacheStart = saplingHeight
+	}
+
+	go common.BlockIngestor(rpcClient, cache, log, cacheStart)
+
+	// Compact transaction service initialization
+	service, err := frontend.NewLwdStreamer(rpcClient, cache, log)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"error": err,
+		}).Fatal("couldn't create backend")
+	}
+
+	// Register service
+	walletrpc.RegisterCompactTxStreamerServer(server, service)
+
+	// Start listening
+	listener, err := net.Listen("tcp", opts.BindAddr)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"bind_addr": opts.BindAddr,
+			"error":     err,
+		}).Fatal("couldn't create listener")
+	}
+
+	// Signal handler for graceful stops
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		s := <-signals
+		log.WithFields(logrus.Fields{
+			"signal": s.String(),
+		}).Info("caught signal, stopping gRPC server")
+		os.Exit(1)
+	}()
+
+	log.Infof("Starting gRPC server on %s", opts.BindAddr)
+
+	err = server.Serve(listener)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"error": err,
+		}).Fatal("gRPC server exited")
+	}
+	return nil
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -74,6 +185,22 @@ func init() {
 	viper.SetDefault("no-tls-very-insecure", false)
 	viper.BindPFlag("cache-size", rootCmd.Flags().Lookup("cache-size"))
 	viper.SetDefault("cache-size", 80000)
+
+	logger.SetFormatter(&logrus.TextFormatter{
+		//DisableColors:          true,
+		FullTimestamp:          true,
+		DisableLevelTruncation: true,
+	})
+
+	onexit := func() {
+		fmt.Printf("Lightwalletd died with a Fatal error. Check logfile for details.\n")
+	}
+
+	log = logger.WithFields(logrus.Fields{
+		"app": "lightwalletd",
+	})
+
+	logrus.RegisterExitHandler(onexit)
 }
 
 // initConfig reads in config file and ENV variables if set.
--- common/common.go
+++ common/common.go
@@ -18,7 +18,7 @@ type Options struct {
 	BindAddr      string `json:"bind_address,omitempty"`
 	TLSCertPath   string `json:"tls_cert_path,omitempty"`
 	TLSKeyPath    string `json:"tls_cert_key,omitempty"`
-	LogLevel      int    `json:"log_level,omitempty"`
+	LogLevel      uint64 `json:"log_level,omitempty"`
 	LogFile       string `json:"log_file,omitempty"`
 	ZcashConfPath string `json:"zcash_conf,omitempty"`
 	VeryInsecure  bool   `json:"very_insecure,omitempty"`
